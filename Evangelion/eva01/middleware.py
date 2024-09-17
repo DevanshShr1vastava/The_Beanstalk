9a70938a49dec4693fdb52794f2e6e8c5d65a41b
Devansh Shrivastava <devansh.shrivastava55@gmail.com>
2024-09-18 01:24:20 +0530

    added the void function remove attempt and question paper function to the middleware part of the code

--- Evangelion/eva01/middleware.py
+++ Evangelion/eva01/middleware.py
@@ -1,6 +1,30 @@
 from django.utils import timezone
-from django.shortcuts import redirect
+from django.shortcuts import redirect,get_object_or_404
 from django.utils.deprecation import MiddlewareMixin
+from .models import questionBank,QuestionPapers,userAttempts, UserProfile
+
+def voidTest(request):
+    if request.user.is_authenticated:
+        user_id = request.user.id
+        username = request.user.username
+    
+    # now fetch the last given questionPaper id
+    # we need to remove the last attempts first 
+    # and then remove the question paper from the questionPapers database
+
+    qpID_to_remove = QuestionPapers.objects.filter(userId = user_id).order_by('qpID').last().qpID
+
+    question_paper_to_remove = get_object_or_404(QuestionPapers, userID = user_id, qpID = qpID_to_remove)
+
+    user_attempt_to_remove = userAttempts.objects.filter(userID = user_id, qpID = question_paper_to_remove)
+    # deleting the attempts first to avoid foreign key constraint error
+    user_attempt_to_remove.delete()
+
+    # now delete the question paper itself second since no foreign key constraint error will come now
+
+    question_paper_to_remove.delete()
+
+
 
 def auto_submit_test(request):
     # Logic to automatically submit the test
